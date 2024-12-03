5e91439373ddf2c2a85622fe5b3bbfcd78019b07
Naim <naimxf@gmail.com>
2024-12-04 00:39:54 +0600

    feat: created api for limited product. also for all products and individual products from mongodb
    Signed-off-by: Naim <naimxf@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -9,6 +10,52 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("server is running");
 });
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sdg7y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+    const db = client.db("gear-arena");
+    const productsCollection = db.collection("all-products");
+
+    app.get("/limited-products", async (req, res) => {
+      const cursor = productsCollection.find().limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const result = await productsCollection.findOne({
+        _id: new ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
+
+    app.get("/all-products", async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
